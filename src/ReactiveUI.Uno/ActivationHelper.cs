e85b8b54825c2a9c4cc5d69661a756901d3bf0cb
Chris Pulman <chris.pulman@yahoo.com>
2022-07-03 01:32:27 +0100

    Initial code base (#2)
    * Initial code base
    
    * Update .gitattributes
    
    * Added missing image
    
    Co-authored-by: Glenn <5834289+glennawatson@users.noreply.github.com>

---
+++ src/ReactiveUI.Uno/ActivationHelper.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2022 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using Splat;
+
+namespace ReactiveUI.Uno
+{
+    internal static class ActivationHelper
+    {
+
+        static ActivationHelper()
+        {
+            if (UnoActivated)
+            {
+                return;
+            }
+
+            UnoActivated = true;
+            Locator.RegisterResolverCallbackChanged(() =>
+            {
+                if (Locator.CurrentMutable is null)
+                {
+                    return;
+                }
+
+                new Registrations().Register((f, t) => Locator.CurrentMutable.RegisterConstant(f(), t));
+            });
+        }
+
+        internal static bool UnoActivated { get; }
+    }
+}
