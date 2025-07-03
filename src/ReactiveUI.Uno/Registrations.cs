b5ac8cb07892f37a8fa10113f4d489793f8d42df
Chris Pulman <chris.pulman@yahoo.com>
2025-07-03 23:39:16 +0100

    Update

--- src/ReactiveUI.Uno/Registrations.cs
+++ src/ReactiveUI.Uno/Registrations.cs
@@ -1,6 +1,6 @@
-// Copyright (c) 2025 .NET Foundation and Contributors. All rights reserved.
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+// Copyright (c) 2025 ReactiveUI and Contributors. All rights reserved.
+// Licensed to reactiveui and contributors under one or more agreements.
+// The reactiveui and contributors licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
 using System;
