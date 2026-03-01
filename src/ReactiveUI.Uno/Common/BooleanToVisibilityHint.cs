f829369af7f010e76f25d92ad6f5dc5b42f987ff
Chris Pulman <chris.pulman@yahoo.com>
2026-03-01 13:52:21 +0000

    Bump package versions and update copyright years
    Update package versions in Directory.Packages.props (ReactiveUI -> 23.1.8, Microsoft.NET.Test.Sdk -> 18.3.0, Microsoft.Testing.Extensions.CodeCoverage -> 18.5.1, TUnit -> 1.17.54, etc.). Update LICENSE and source/test file copyright headers to 2021 - 2026 and refresh stylecop copyright text. Remove the AppLocatorViewLocator example and its registration in the SQLiteStudio sample App.xaml.cs.

--- src/ReactiveUI.Uno/Common/BooleanToVisibilityHint.cs
+++ src/ReactiveUI.Uno/Common/BooleanToVisibilityHint.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2025 ReactiveUI and Contributors. All rights reserved.
+﻿// Copyright (c) 2021 - 2026 ReactiveUI and Contributors. All rights reserved.
 // Licensed to reactiveui and contributors under one or more agreements.
 // The reactiveui and contributors licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
